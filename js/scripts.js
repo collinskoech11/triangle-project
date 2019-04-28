eb28226bbc7032627a488d8ff2d63d04039ef81f
goldangera <nkirotegolda6888@gmail.com>
2019-04-28 19:46:53 +0300

    made correct spacing in the codes

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,12 @@ var track = function(first, second, third){
   var secondlength = document.getElementById("secondl").value;
   var thirdlength = document.getElementById("thirdl").value;
 
-  // Converting input to intergers
+  // Converting input to intergers 
   var first = parseInt(firstlength);
   var second = parseInt(secondlength);
   var third = parseInt(thirdlength);
 
-  if((first + second > third) && (first + third > second) && (third + second > first)){
+   if((first + second > third) && (first + third > second) && (third + second > first)){
      //Checking for an equilateral triangle
     if(first === second && first === third){
       var triangle = "Equilateral";
@@ -21,7 +21,8 @@ var track = function(first, second, third){
       document.getElementById("result").innerHTML = "The dimensions " + eqArray + " form an " + triangle + " triangle!";
 
     }
-  //Checking for an Isosceles triangle
+
+    //Checking for an Isosceles triangle
     else if(first === second || first === third || second === third){
       var triangle = "Isosceles";
       var isArray = [];
@@ -34,18 +35,19 @@ var track = function(first, second, third){
     }
 
     //Checking for a scalene triangle
-   else{
-     var triangle = "Scalene";
-     var scArray = [];
-     scArray.push(first);
-     scArray.push(second);
-     scArray.push(third);
+    else{
+      var triangle = "Scalene";
+      var scArray = [];
+      scArray.push(first);
+      scArray.push(second);
+      scArray.push(third);
 
-     document.getElementById("result").innerHTML = "The dimensions " + scArray + " form a " + triangle + " triangle!";
+      document.getElementById("result").innerHTML = "The dimensions " + scArray + " form a " + triangle + " triangle!";
 
-   }
+    }
+  }
 
-   //Result if the triangle test has failed to print no triangle
+  //Result if the triangle test has failed to print no triangle
    else{
      var triangle = "NO triangle at all";
      var noArray = [];
@@ -55,3 +57,9 @@ var track = function(first, second, third){
 
      document.getElementById("result").innerHTML = "The dimensions " + noArray + " form " + triangle;
 }
+ }
+
+//Function to clear the page of images and result message
+ var reseting = function(){
+   document.getElementById("result").innerHTML = "";
+ }
