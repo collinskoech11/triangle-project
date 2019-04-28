773e1ef0ad2a73ce322caa40702cc3158aee8bb9
goldangera <nkirotegolda6888@gmail.com>
2019-04-28 17:37:23 +0300

    create a var called firstlength,secondlength,thirdlengh and getElementById that were specified in the HTML

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//Business logic
+var track = function(first, second, third){
+  var firstlength = document.getElementById("firstl").value;
+  var secondlength = document.getElementById("secondl").value;
+  var thirdlength = document.getElementById("thirdl").value;
+
+  // Converting input to intergers
+  var first = parseInt(firstlength);
+  var second = parseInt(secondlength);
+  var third = parseInt(thirdlength);
